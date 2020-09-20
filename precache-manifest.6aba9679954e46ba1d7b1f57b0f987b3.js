0f048cecd27b6bd4f0d8b215b1be2352a80066ec
Ron Rieck <ron.rieck@gmx.de>
2020-09-20 09:24:04 +0200

    Updates

---
+++ precache-manifest.6aba9679954e46ba1d7b1f57b0f987b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fe296d1c7f1d53ef3c736e88e09b42",
+    "url": "/frontend-portfolio/index.html"
+  },
+  {
+    "revision": "496b4196c71d0ff1b0d5",
+    "url": "/frontend-portfolio/static/css/main.900e8949.chunk.css"
+  },
+  {
+    "revision": "f9ab711283e17c6c7a04",
+    "url": "/frontend-portfolio/static/js/2.3aef8fa4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-portfolio/static/js/2.3aef8fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496b4196c71d0ff1b0d5",
+    "url": "/frontend-portfolio/static/js/main.6698f0e7.chunk.js"
+  },
+  {
+    "revision": "be66f527075d1a4f5bbf",
+    "url": "/frontend-portfolio/static/js/runtime-main.23cbe1ed.js"
+  },
+  {
+    "revision": "bd873d138ce65030df83fb0018f9afb6",
+    "url": "/frontend-portfolio/static/media/aboutme.bd873d13.png"
+  }
+]);
